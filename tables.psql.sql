18f8bfb876af37e0cd362b93d99cd412d5f04710
Antoine <antoine.montier@sfr.fr>
2023-01-05 09:51:48 +0100

    added Shareholder table

--- tables.psql.sql
+++ tables.psql.sql
@@ -4,3 +4,11 @@ CREATE TABLE IF NOT EXISTS Incorparation (
     inc_address VARCHAR(255) NOT NULL,
     inc_captial INTEGER NOT NULL
 );
+
+
+CREATE TABLE IF NOT EXISTS Shareholder (
+    sha_id SERIAL PRIMARY KEY,
+    sha_lastname VARCHAR(255) NOT NULL,
+    sha_firstname VARCHAR(255) NOT NULL,
+    sha_address VARCHAR(255) NOT NULL
+);
