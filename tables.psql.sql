a2696f9580ef79f5cb0a82bce5c0511b34f75760
Antoine <antoine.montier@sfr.fr>
2023-01-05 09:53:25 +0100

    added Posseses table

--- tables.psql.sql
+++ tables.psql.sql
@@ -12,3 +12,13 @@ CREATE TABLE IF NOT EXISTS Shareholder (
     sha_firstname VARCHAR(255) NOT NULL,
     sha_address VARCHAR(255) NOT NULL
 );
+
+
+CREATE TABLE IF NOT EXISTS Possesses (
+    inc_id INTEGER NOT NULL,
+    sha_id INTEGER NOT NULL,
+    action_number INTEGER check (action_number >= 0),
+    PRIMARY KEY (inc_id, sha_id),
+    FOREIGN KEY (inc_id) REFERENCES Incorparation(inc_id),
+    FOREIGN KEY (sha_id) REFERENCES Shareholder(sha_id)
+);
